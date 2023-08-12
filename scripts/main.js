8adfe132aa2a2d0ae4e03421705b093b6c063f95
Manuel Rodriguez <manuelrodriguezcontactolaboral@gmail.com>
2023-08-12 16:32:08 -0300

    Arregle un error del input y la forma en la que se mostraban los productos encontrados

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
+//Creacion del codigo HTML basico
+
+
+
+
 const inputBusqueda = document.getElementById("inputBusqueda");
 const botonBusqueda = document.getElementById("botonBusqueda");
+const seccionBusqueda = document.getElementById("sectionBusqueda");
+const seccionFav = document.getElementById("seccionFav")
 const resultadoBusqueda = document.getElementById("resultadoBusqueda");
 const listaFavoritos = document.getElementById("listaDeFavoritos");
 
@@ -12,7 +19,7 @@ fetch('products.json') //para traer los datos del archivo JSON se usa fetch
   })
   .catch(error => console.error('La busqueda del JSON no esta funcionando!', error)); //si no funciona tira este mensaje
 
-inputBusqueda.addEventListener('change', empezarBusqueda)
+inputBusqueda.addEventListener('change', empezarBusqueda) //si hace click en el boton o enter busca el producto
 botonBusqueda.addEventListener('click', empezarBusqueda)
 
 function empezarBusqueda() {
@@ -22,13 +29,20 @@ function empezarBusqueda() {
 }
 
 function filtrarProductos(ingresoInput) {
-  if (ingresoInput === "") { //para que no muestre todos los resultados sin sentido
+  if (ingresoInput === "") { //si no ingresa nada, no se hace nada
     return [];
   }
-  return listaProductos.filter(productoX => productoX.nombre.toUpperCase().includes(ingresoInput.toUpperCase()));
+  const productosFiltrados = listaProductos.filter(productoX => productoX.nombre.toUpperCase().includes(ingresoInput.toUpperCase()));
+  if (productosFiltrados.length === 0) { //si no se encuentra ningun producto devuelve un ALERT
+    alert("No se encontro ningun producto con el nombre: " + ingresoInput.toUpperCase());
+  }
+  return productosFiltrados;
 }
 
-function mostrarResultados(listaProductos) { //muestra los productos encontrados
+ //arregla el error de que cuando no se ingresaba nada se mostraban todos los productos(op.t if else)
+
+function mostrarResultados(listaProductos) {
+  seccionBusqueda.innerHTML = `<h3>Productos encontrados:</h3>`;
   resultadoBusqueda.innerHTML = "";
   listaProductos.forEach(productoX => {
     const productoEncontradoDiv = document.createElement('div');
@@ -50,6 +64,7 @@ function mostrarResultados(listaProductos) { //muestra los productos encontrados
       }
     });
     resultadoBusqueda.appendChild(productoEncontradoDiv)
+    seccionBusqueda.appendChild(resultadoBusqueda)
   });
 }
 
@@ -73,6 +88,7 @@ window.addEventListener('load', function(){
 });
 
 function mostrarFavoritos(fav) { //construye igual que cuando buscas pero ahora en la pagina de favoritos
+  seccionFav.innerHTML = `<h3>Tus favoritos:</h3>`;
   listaFavoritos.innerHTML = ""; //esto tira un error cuando volves a index!!!!(ARREGLAR)
   fav.forEach(productoX => {
     const productoFavorito = document.createElement('div');
@@ -91,6 +107,7 @@ function mostrarFavoritos(fav) { //construye igual que cuando buscas pero ahora 
     });
 
     listaFavoritos.appendChild(productoFavorito);
+    seccionFav.appendChild(listaFavoritos);
   })
 }
 
